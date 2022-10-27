f3bae30513b7b6d8a3c6597e3ed287d3764eabc4
Vania_Dmitriienko <dmitrienko2706@gmail.com>
2022-10-27 19:28:17 +0300

    add task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Container } from './feedback/FeedbackStyled';
+import FeedbackStats from './feedback/FeedbackStats';
+import Feedbacks from './feedback/Feedback';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  changeState = evt => {
+    const key = evt.target.dataset.type;
+    const stateObject = {};
+    stateObject[key] = this.state[key] + 1;
+    this.setState(stateObject);
+  };
+
+  countTotalFeedback = (good, neutral, bad) => {
+    return good + neutral + bad;
+  };
+  countPositiveTotalPercantage = (total, good) => {
+    return total ? Math.ceil((good * 100) / total) : 0;
+  };
+
+  render() {
+    const good = this.state.good;
+    const neutral = this.state.neutral;
+    const bad = this.state.bad;
+    return (
+      <Container>
+        <Feedbacks
+          options={Object.keys(this.state)}
+          changeState={this.changeState}
+        ></Feedbacks>
+        <FeedbackStats
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback(good, neutral, bad)}
+          goodPercentage={this.countPositiveTotalPercantage(
+            this.countTotalFeedback(good, neutral, bad),
+            good
+          )}
+        ></FeedbackStats>
+      </Container>
+    );
+  }
+}
+export default App;
