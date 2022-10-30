ceb8f98b8b3bc44aed3c510ff98b98fe16a2d532
Vania_Dmitriienko <dmitrienko2706@gmail.com>
2022-10-30 21:05:26 +0200

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Container } from './feedback/FeedbackStyled';
-import FeedbackStats from './feedback/FeedbackStats';
-import Feedbacks from './feedback/Feedback';
+import { Container } from './Feedback/FeedbackStyled';
+import FeedbackStats from './Feedback/FeedbackStats';
+import Feedbacks from './Feedback/Feedback';
 
 class App extends Component {
   state = {
